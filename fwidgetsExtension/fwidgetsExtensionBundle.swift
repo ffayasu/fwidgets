abed62dc417fb498f86cb52ce68120752152e854
fayasu <ffayasuu@gmail.com>
2026-09-24 00:15:55 +0400

    initial widgets

---
+++ fwidgetsExtension/fwidgetsExtensionBundle.swift
@@ -0,0 +1,17 @@
+//
+//  fwidgetsExtensionBundle.swift
+//  fwidgetsExtension
+//
+//  Created by Андрей Широбоков on 23.09.2026.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct fwidgetsExtensionBundle: WidgetBundle {
+    var body: some Widget {
+        fwidgetsExtension()
+        fwidgetsExtensionControl()
+    }
+}
